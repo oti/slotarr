55599decaf37c79fd1a76acfaf2a20ac28161fbf
oti <otiext@gmail.com>
2019-04-19 15:24:36 +0900

    10連ガチャ機能を追加した

--- src/script/class/Slotarr.ts
+++ src/script/class/Slotarr.ts
@@ -3,6 +3,7 @@ export default class Slotarr {
   $input: HTMLTextAreaElement
   $output: HTMLDivElement
   $execute: HTMLButtonElement
+  $juuRen: HTMLButtonElement
   defaultSlotAmount: number
 
   constructor() {
@@ -10,6 +11,7 @@ export default class Slotarr {
     this.$input = document.querySelector('#input') as HTMLTextAreaElement
     this.$output = document.querySelector('#output') as HTMLDivElement
     this.$execute = document.querySelector('#execute') as HTMLButtonElement
+    this.$juuRen = document.querySelector('#juuRen') as HTMLButtonElement
     this.defaultSlotAmount = 3
   }
 
@@ -30,7 +32,7 @@ export default class Slotarr {
   }
 
   get shuffledArray() {
-    let array = []
+    let array: string[] = []
 
     for (let i=0; i<this.slotAmount; i++) {
       array.push(this.seedArray[this.idx]);
@@ -39,13 +41,53 @@ export default class Slotarr {
     return array
   }
 
-  setResult(array: string[]) {
-    this.$output.textContent = array.join(' / ')
+  get shuffled10Array() {
+    let array10: string[][] = []
+    
+    for (let i=0; i<10; i++) {
+      let array1: string[] = []
+      for (let i=0; i<this.slotAmount; i++) {
+        array1.push(this.seedArray[this.idx]);
+      }
+      array10.push(array1)
+    }
+
+    return array10
+  }
+
+  isEqual(target: string[]) {
+    for (let i=0; i<this.slotAmount; i++) {
+      if (target[0] !== target[i]) {
+        return false
+      }
+    }
+    return true
+  }
+
+  setResult(result: string[]) {
+    this.$output.textContent = result.join(' / ')
+  }
+
+  setJuuRenResult(result: string[][]) {
+    this.$output.textContent = ''
+    result.forEach((one: string[], i: number, arr: string[][]) => {
+      let $p: HTMLParagraphElement = document.createElement('p')
+      $p.textContent = i+1 + ': ' + one.join(' / ')
+
+      if (this.isEqual(one)) {
+        $p.classList.add('u-equal')
+      }
+
+      this.$output.appendChild($p)
+    })
   }
 
   dispatch() {
     this.$execute.addEventListener('click', (ev: Event) => {
       this.setResult(this.shuffledArray)
+    })
+    this.$juuRen.addEventListener('click', (ev: Event) => {
+      this.setJuuRenResult(this.shuffled10Array)
     })
   }
 }
