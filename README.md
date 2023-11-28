7bd6896d390b1ec49192104321a9dcf420e23bcc
oti <otiext@gmail.com>
2023-11-28 13:37:29 +0900

    feat: parcel をやめて vanilla に置き換えた

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 
 ![カバー画像](src/image/og-image.png)
 
-https://slotarr.dskd.jp/  
-[![Netlify Status](https://api.netlify.com/api/v1/badges/a9b47b26-362d-47ca-a0bf-4230ea039ad9/deploy-status)](https://app.netlify.com/sites/slotarr/deploys)
+https://oti.github.io/slotarr/
 
 半角スペース区切りの単語をスロットするだけのウェブアプリ。3 つ揃うとテキストに色がつきます。
 
 行きたいお店が決められない時に便利です。
 
-## ライセンス
+## License
 
-Copyright (c) oti. Under the [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/deed.ja).
+MIT.
--- docs/script/Slotarr.mjs
+++ docs/script/Slotarr.mjs
@@ -4,31 +4,14 @@ const dict = {
 };
 
 export default class Slotarr {
-  $amount: HTMLInputElement;
-  $separaters: NodeListOf<HTMLInputElement>;
-  $input: HTMLTextAreaElement;
-  $outputDetail: HTMLDivElement;
-  $outputSummary: HTMLDivElement;
-  $execute: HTMLButtonElement;
-  $multiple: HTMLButtonElement;
-  multipleAmount: number;
-  splitString: string;
-  joinString: string;
-
   constructor() {
-    this.$amount = document.querySelector("#amount") as HTMLInputElement;
-    this.$separaters = document.querySelectorAll(
-      '[name="separater"]'
-    ) as NodeListOf<HTMLInputElement>;
-    this.$input = document.querySelector("#input") as HTMLTextAreaElement;
-    this.$outputDetail = document.querySelector(
-      "#outputDetail"
-    ) as HTMLDivElement;
-    this.$outputSummary = document.querySelector(
-      "#outputSummary"
-    ) as HTMLDivElement;
-    this.$execute = document.querySelector("#execute") as HTMLButtonElement;
-    this.$multiple = document.querySelector("#multiple") as HTMLButtonElement;
+    this.$amount = document.querySelector("#amount");
+    this.$separaters = document.querySelectorAll('[name="separater"]');
+    this.$input = document.querySelector("#input");
+    this.$outputDetail = document.querySelector("#outputDetail");
+    this.$outputSummary = document.querySelector("#outputSummary");
+    this.$execute = document.querySelector("#execute");
+    this.$multiple = document.querySelector("#multiple");
     this.multipleAmount = 100;
     this.splitString = dict.split_string[0];
     this.joinString = dict.join_string;
@@ -45,8 +28,8 @@ export default class Slotarr {
    * @get 区切り文字を返す
    */
   get separater() {
-    let value: number = 0;
-    this.$separaters.forEach((elem: HTMLInputElement, i: number) => {
+    let value = 0;
+    this.$separaters.forEach((elem) => {
       if (elem.checked) {
         value = parseInt(elem.value, 10);
         return;
@@ -80,7 +63,7 @@ export default class Slotarr {
    * seedArrayで返ってくる配列を元にスロットの数だけランダムに抽出した配列を返す
    */
   get shuffledArray() {
-    let array: string[] = [];
+    let array = [];
 
     for (let i = 0; i < this.slotAmount; i++) {
       array.push(this.seedArray[this.idx]);
@@ -93,10 +76,10 @@ export default class Slotarr {
    * seedArrayで返ってくる配列を元にスロットの数だけランダムに抽出した配列を n 個入った二次元配列を返す
    */
   get shuffledMultipleArray() {
-    let multiArray: string[][] = [];
+    let multiArray = [];
 
     for (let i = 0; i < this.multipleAmount; i++) {
-      let array: string[] = [];
+      let array = [];
       for (let i = 0; i < this.slotAmount; i++) {
         array.push(this.seedArray[this.idx]);
       }
@@ -109,7 +92,7 @@ export default class Slotarr {
   /**
    * 引数に渡した配列の中身が全て同じかどうか返す
    */
-  isEqual(target: string[]) {
+  isEqual(target) {
     for (let i = 0; i < this.slotAmount; i++) {
       if (target[0] !== target[i]) {
         return false;
@@ -129,14 +112,14 @@ export default class Slotarr {
   /**
    * シャッフル結果を文字列にしてテキストノードに挿入する
    */
-  setResult(result: string[]) {
+  setResult(result) {
     this.$outputDetail.textContent = result.join(this.joinString);
   }
 
   /**
    * シャッフル結果を文字列にしてテキストノードに挿入する
    */
-  setMultipleResult(result: string[][]) {
+  setMultipleResult(result) {
     const formattedResult = result.map((one) => ({
       text: one.join(this.joinString),
       isEqual: this.isEqual(one),
@@ -163,7 +146,7 @@ export default class Slotarr {
       .sort((a, b) => b.count - a.count);
 
     formattedResult.forEach(
-      (one: { text: string; isEqual: boolean }, i: number) => {
+      (one, i) => {
         const $p = document.createElement("p");
         $p.textContent = String(i + 1).padStart(2, "0") + ": " + one.text;
         $p.classList.toggle("-equal", one.isEqual);
@@ -171,7 +154,7 @@ export default class Slotarr {
       }
     );
 
-    summary.forEach((one: { text: string; count: number }) => {
+    summary.forEach((one) => {
       const $p = document.createElement("p");
       $p.textContent = one.text + ": " + one.count + "回";
       this.$outputSummary.appendChild($p);
@@ -182,12 +165,12 @@ export default class Slotarr {
    * イベントのバインド
    */
   dispatch() {
-    this.$execute.addEventListener("click", (ev: Event) => {
+    this.$execute.addEventListener("click", () => {
       this.resetResult();
       this.setResult(this.shuffledArray);
     });
 
-    this.$multiple.addEventListener("click", (ev: Event) => {
+    this.$multiple.addEventListener("click", () => {
       this.resetResult();
       this.setMultipleResult(this.shuffledMultipleArray);
     });
